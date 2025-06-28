7dc976e1ea4a44b14807ac963d17738426bb679c
Muhammed Sanjid <sanjid357@gmail.com>
2025-06-29 01:39:30 +0530

    added_new_origins

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 
 // 1. CORS 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173',"https://ai-meal-planner-m-sanjids-projects.vercel.app/", 'https://befitai.sanjid.shop', 'http://127.0.0.1:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
